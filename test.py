f42fc5eafb9893f8e548853e65ff65b73939cee8
petkodimitrov <petko@nitropack.com>
2022-01-24 12:06:05 +0200

    Troubleshoot SNYK

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ from art import text2art
 
 def outputtext():
     '''Outputs some text'''
-    custom_text = text2art("УРА", font='block', chr_ignore=True)
+    custom_text = text2art("TELERIK ACADEMY", font='block', chr_ignore=True)
     print('Content-Type: text/plain')
     print('')
-    print('This is my test!')
+    print('This is a successful test!')
     print(custom_text)
 
 
