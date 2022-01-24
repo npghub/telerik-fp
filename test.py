95363575138a7b55cfa2f7a7a5f08ff3b1c5c958
petkodimitrov <petko@nitropack.com>
2022-01-24 10:33:49 +0200

    Fix formatting issues

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from art import text2art
 
+
 def outputtext():
-    '''Takes in a number n, returns the square of n'''
-    custom_text = text2art("УРА",font='block',chr_ignore=True)
+    '''Outputs some text'''
+    custom_text = text2art("УРА", font='block', chr_ignore=True)
     print('Content-Type: text/plain')
     print('')
     print('This is my test!')
     print(custom_text)
+
 
 outputtext()
